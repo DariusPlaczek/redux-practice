2c2608bfd3a9efb6bfe5dc6bd95ecd0cfea22684
Darius Placzek <dariusplaczek@gmail.com>
2021-01-06 22:09:58 +0100

    first commit

---
+++ src/components/redux.js
@@ -0,0 +1,38 @@
+const COUNTER_INC = 'counter/COUNTER_INC';
+const COUNTER_DEC = 'counter/COUNTER_DEC';
+const COUNTER_DEL = 'counter/COUNTER_DEL';
+
+export function inc() {
+    return{
+        type: COUNTER_INC,
+    }
+}
+
+export function dec() {
+    return{
+        type: COUNTER_DEC
+    }
+}
+
+export const del = () => ({
+    type: COUNTER_DEL
+});
+
+const INITIAL_STATE = {
+    count: 0
+}
+
+export function reducer(state = INITIAL_STATE, action) {
+    switch (action.type) {
+        case COUNTER_INC:
+            return {...state, count: state.count + 1}
+        case COUNTER_DEC:
+            return {...state, count: state.count - 1}
+        case COUNTER_DEL:
+            return {...state, count: 0 }
+        default:
+            return state
+    }
+}
+
+
